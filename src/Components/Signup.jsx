435d2ac835b434329465310e6212056c99e1f2bf
Oluwseun Adeyeri <adeyeriseun0@gmail.com>
2024-05-15 23:19:26 +0100

    created a signup page for my schoola applicaiton

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
     <>
     <main className='flex justify-center py-5'>
-      <section className='shadow-lg p-5 w96 rounded-lg'>
+      <form  className='shadow-lg p-5 w96 rounded-lg border-t-2 border-green-500 bg-white'>
         <h1 className='text-2xl font-bold poppins-bold'>Sign up</h1>
       <div className='w-full'>
         <input type="text" placeholder='First Name' className='w-full outline-none p-4 poppins-regular'/>
@@ -18,10 +19,13 @@ const Signup = () => {
       <div className='w-full'>
         <input type="passwird" placeholder='Password' className='w-full outline-none p-4 poppins-regular'/>
       </div>
-      <div>
-        <button className='bg-green-500 w-full p-2 text-white rounded font-bold poppins-bold'>Sign up</button>
+      <div className='my-5'>
+        <button type='submit' className='bg-green-500 w-full p-2 text-white rounded font-bold poppins-bold'>Sign up</button>
       </div>
-      </section>
+      <div>
+        <p className='poppins-regular moreinfo'>Already have an account? <Link to="/login">Login</Link></p>
+      </div>
+      </form>
     </main>
     </>
   )
