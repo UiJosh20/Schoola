f7c934f412cca3fe460fe51baadf551fc933ffc5
Oluwseun Adeyeri <adeyeriseun0@gmail.com>
2024-05-15 23:09:40 +0100

    signup ui coming to live

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -4,18 +4,22 @@ const Signup = () => {
   return (
     <>
     <main className='flex justify-center py-5'>
-      <section className='shadow-lg p-5 w96'>
+      <section className='shadow-lg p-5 w96 rounded-lg'>
+        <h1 className='text-2xl font-bold poppins-bold'>Sign up</h1>
       <div className='w-full'>
-        <input type="text" placeholder='First Name' className='w-full outline-none p-4'/>
+        <input type="text" placeholder='First Name' className='w-full outline-none p-4 poppins-regular'/>
       </div>
       <div className='w-full'>
-        <input type="text" placeholder='Last Name' className='w-full outline-none p-4'/>
+        <input type="text" placeholder='Last Name' className='w-full outline-none p-4 poppins-regular'/>
       </div>
       <div className='w-full'>
-        <input type="email" placeholder='Email address' className='w-full outline-none p-4'/>
+        <input type="email" placeholder='Email address' className='w-full outline-none p-4 poppins-regular'/>
       </div>
       <div className='w-full'>
-        <input type="passwird" placeholder='Password' className='w-full outline-none p-4'/>
+        <input type="passwird" placeholder='Password' className='w-full outline-none p-4 poppins-regular'/>
+      </div>
+      <div>
+        <button className='bg-green-500 w-full p-2 text-white rounded font-bold poppins-bold'>Sign up</button>
       </div>
       </section>
     </main>
